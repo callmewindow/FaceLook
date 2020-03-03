5fa0be059d276cdb2fabb244eb95551ffa36bbdb
ClementineZ <1415162394@qq.com>
2020-03-03 23:50:10 +0800

    ?

--- FrontEnd/Elements/Button.py
+++ FrontEnd/Elements/Button.py
@@ -3,5 +3,10 @@ import pygame
 class Button():
     def __init__(self,process):
         Element.__init__(self,process)
-        
+    def onClick(self):
+        pass
+    def update(self):
+        pass
+    def onHover(self):
+        pass
         
