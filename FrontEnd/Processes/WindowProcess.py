f6f5a23842d9f776d27b5fe9f3042fdde24a3413
ClementineZ <1415162394@qq.com>
2020-03-04 00:28:37 +0800

    Abstract Windowprocess

---
+++ FrontEnd/Processes/WindowProcess.py
@@ -0,0 +1,38 @@
+import pygame
+from queue import Queue
+pygame.init()
+class WindowProcess():
+    def __init__(self,data,window):        
+        self.window = window
+        self.FPS = 60 
+        self.go = True    
+        self.data = data
+        self.actionList = []
+        self.requestQueue = Queue()
+        self.messageQueue = Queue()
+        self.bet = None
+    def run(self):
+        while self.go:
+            for event in pygame.event.get():
+                if (event.type==pygame.constants.QUIT):
+                    pygame.quit() 
+                    self.go = False 
+                    return 
+                self.window.getEvent(event)
+            try:
+                message = self.messageQueue.get(block=False)
+                self.window.getMessage(message)
+            except:
+                pass
+            for action in self.actionList:
+                self.doAction(action)
+            self.actionList.clear()   
+            self.window.display()
+            pygame.display.update()
+            self.window.FPSClock.tick(self.FPS)
+    def addAction(self,action):
+        self.actionList.append(action)
+    def doAction(self,action):
+        pass
+    def stop(self):
+        self.go = False
