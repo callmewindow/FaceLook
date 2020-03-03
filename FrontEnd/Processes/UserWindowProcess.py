ad2cdf2caa2734e85602db7a85452efedd9d60ca
ClementineZ <1415162394@qq.com>
2020-03-04 00:41:22 +0800

    return data

--- FrontEnd/Processes/UserWindowProcess.py
+++ FrontEnd/Processes/UserWindowProcess.py
@@ -3,8 +3,6 @@ from FrontEnd.Elements.UserWindow import UserWindow
 from FrontEnd.Processes.WindowProcess import WindowProcess
 from BackEnd.BackEndThread import BackEndThread
 from BackEnd.BackEndStaticMethods import *
-
-    
 class UserWindowProcess(WindowProcess):
     def __init__(self,data): 
         WindowProcess.__init__(self,data,UserWindow(self))       
@@ -15,4 +13,5 @@ def main(data):
     print(data.login)
     uwp = UserWindowProcess(data)
     uwp.run()
+    return data
 
