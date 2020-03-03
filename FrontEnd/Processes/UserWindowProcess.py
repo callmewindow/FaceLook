70ad31345bdaf67244811fdcb10dac863e78019c
ClementineZ <1415162394@qq.com>
2020-03-04 00:46:41 +0800

    Remove some space

--- FrontEnd/Processes/UserWindowProcess.py
+++ FrontEnd/Processes/UserWindowProcess.py
@@ -6,8 +6,7 @@ from BackEnd.BackEndStaticMethods import *
 class UserWindowProcess(WindowProcess):
     def __init__(self,data): 
         WindowProcess.__init__(self,data,UserWindow(self))       
-        self.bet = BackEndThread(self.messageQueue,self.data)        
-
+        self.bet = BackEndThread(self.messageQueue,self.data)
 
 def main(data):
     print(data.login)
