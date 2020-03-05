294aa946cec776ef071c6c0e93e55ef251de3216
ClementineZ <1415162394@qq.com>
2020-03-05 17:48:12 +0800

    Test

--- FrontEnd/Processes/UserWindowProcess.py
+++ FrontEnd/Processes/UserWindowProcess.py
@@ -1,10 +1,7 @@
 import pygame
 from FrontEnd.Elements.UserWindow import UserWindow
 from FrontEnd.Processes.WindowProcess import WindowProcess
-from BackEnd.BackEndThread import BackEndThread
 from BackEnd.BackEndStaticMethods import *
 class UserWindowProcess(WindowProcess):
     def __init__(self,data,RQ,MQ,bet): 
         WindowProcess.__init__(self,data,RQ,MQ,bet,UserWindow(self))       
-
-
