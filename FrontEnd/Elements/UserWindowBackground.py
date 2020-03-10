a91e693c07eaf396c59f897a0c036532b80e9f26
ClementineZ <1415162394@qq.com>
2020-03-10 19:48:01 +0800

    可以用中文吗

--- FrontEnd/Elements/UserWindowBackground.py
+++ FrontEnd/Elements/UserWindowBackground.py
@@ -1,6 +1,7 @@
 from FrontEnd.Elements.Element import Element
 from FrontEnd.Elements.Avatar import Avatar
 from FrontEnd.Elements.FriendList import *
+from FrontEnd.Elements.text_default import *
 import pygame
 class UserWindowBackground(Element):
     def __init__(self,process):
@@ -10,6 +11,8 @@ class UserWindowBackground(Element):
         self.location = (0,0)
 
     def init(self):
+        self.logo = self.createChild(text_default,(0,0),'0 Message(s), 0 Invitation(s)',(0,0,0))
+        self.logo.alignCenter((175,50))
         self.friendList = self.createChild(FriendList,(0,100),self.process.data.friendList)
 
         
