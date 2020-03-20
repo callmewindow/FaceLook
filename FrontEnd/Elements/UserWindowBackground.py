bbf1cca4e252dbac79b260278be3bd85fdb527b9
KosakaKotori <1411607509@qq.com>
2020-03-20 18:10:02 +0800

    修改子元素位置

--- FrontEnd/Elements/UserWindowBackground.py
+++ FrontEnd/Elements/UserWindowBackground.py
@@ -4,6 +4,7 @@ from FrontEnd.Elements.FriendList import *
 from FrontEnd.Elements.text_default import *
 from FrontEnd.Elements.SelfInfo import *
 from FrontEnd.Elements.SearchBar import *
+from FrontEnd.Elements.MenuBar import *
 import pygame
 
 
@@ -17,6 +18,7 @@ class UserWindowBackground(Element):
     def init(self):
         self.selfInfo = self.createChild(SelfInfo, (0, 0), self.process.data.user)
         self.searchBar = self.createChild(SearchBar, (0, 100))
+        self.menuBar = self.createChild(MenuBar, (0, 155))
         # self.logo = self.createChild(text_default, (0, 0), '0 Message(s), 0 Invitation(s)', (0, 0, 0))
         # self.logo.alignCenter((175, 50))
-        self.friendList = self.createChild(FriendList, (0, 155), self.process.data.friendList)
+        self.friendList = self.createChild(FriendList, (0, 200), self.process.data.friendList)
