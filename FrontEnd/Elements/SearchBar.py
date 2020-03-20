008aadd8d9b9223b3136cc4898a30ccbcbecd37a
KosakaKotori <1411607509@qq.com>
2020-03-20 18:09:21 +0800

    修改子元素位置

--- FrontEnd/Elements/SearchBar.py
+++ FrontEnd/Elements/SearchBar.py
@@ -5,7 +5,7 @@ from FrontEnd.Elements.Inputbox_default import Inputbox_default
 
 class SearchBar(Element):
     searchIcon = pygame.image.load('./resources/searchicon.png')
-    image = pygame.Surface((350, 100))
+    image = pygame.Surface((350, 55))
     image.fill((255, 255, 255))
 
     def __init__(self, process, location):
@@ -24,3 +24,11 @@ class SearchBar(Element):
                 surface.blit(child.display(), child.location)
         surface.blit(self.icon, (10, 10))
         return surface
+
+    def getEvent(self, event):
+        if event.type == pygame.constants.MOUSEBUTTONDOWN or event.type == pygame.constants.MOUSEMOTION:
+            event.pos = (event.pos[0] - self.location[0], event.pos[1] - self.location[1])
+        for child in self.childs:
+            child.getEvent(event)
+        if event.type == pygame.constants.MOUSEBUTTONDOWN or event.type == pygame.constants.MOUSEMOTION:
+            event.pos = (event.pos[0] + self.location[0], event.pos[1] + self.location[1])
