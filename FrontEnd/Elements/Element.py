467042d3366586006b01095547927c305e3d113e
KosakaKotori <1411607509@qq.com>
2020-04-20 22:28:29 +0800

    代码格式化

--- FrontEnd/Elements/Element.py
+++ FrontEnd/Elements/Element.py
@@ -1,7 +1,10 @@
 import pygame
-class Element():
+
+
+class Element:
     listens = []
-    def __init__(self,process):
+
+    def __init__(self, process):
         self.parent = None
         self.childs = []
         self.state = 0
@@ -9,30 +12,38 @@ class Element():
         self.surface = None
         self.process = process
         self.active = True
-    def getEvent(self,event):
+
+    def getEvent(self, event):
         for child in self.childs:
             if child.active:
                 child.getEvent(event)
-    def createChild(self,childType,*args,**kwargs):
-        child = childType(self.process,*args,**kwargs)
+
+    def createChild(self, childType, *args, **kwargs):
+        child = childType(self.process, *args, **kwargs)
         self.childs.append(child)
         return child
+
     def update(self):
         for child in self.childs:
             if child.active:
                 child.update()
+
     def display(self):
-        surface = pygame.Surface.copy(self.surface)
+        surface = self.surface.copy()
         for child in self.childs:
             if child.active:
-                surface.blit(child.display(),child.location)
+                surface.blit(child.display(), child.location)
         return surface
-    def switchState(self,state):
+
+    def switchState(self, state):
         self.state = state
         self.counter = 0
+
     def enable(self):
         self.active = True
+
     def disable(self):
         self.active = False
-    def getMessage(self,message):
+
+    def getMessage(self, message):
         pass
