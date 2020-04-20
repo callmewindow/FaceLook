01facf58a22d4a0e9711f805f4fd4d24a7fe2285
KosakaKotori <1411607509@qq.com>
2020-04-20 22:29:47 +0800

    新增列表切换、右键菜单

--- FrontEnd/Elements/FriendList.py
+++ FrontEnd/Elements/FriendList.py
@@ -1,7 +1,7 @@
 from FrontEnd.Elements.Element import Element
 from FrontEnd.Elements.Avatar import Avatar
 from FrontEnd.Elements.text_default import text_default
-from FrontEnd.Elements.MenuBar import MenuBar
+from FrontEnd.Elements.RightClickMenu import RightClickMenu
 import pygame
 
 
@@ -19,16 +19,18 @@ class FriendBlock(Element):
     image_onClick = pygame.Surface((350, 100))
     image_onClick.fill((240, 240, 240))
 
-    def __init__(self, process, location, user, type):
+    def __init__(self, process, location, user, block_type, menu):
         Element.__init__(self, process)
         self.user = user
-        self.avatar = self.createChild(Avatar, (25, 15), self.user.avatar)
-        userStateText = ' (online)' if self.user.state == 1 else ' (offline)'
-        self.nicknameText = self.createChild(text_default, (120, 38), self.user.nickname + userStateText, (0, 0, 0))
+        self.rightClickMenu = menu
+        self.avatar = self.createChild(Avatar, (25, 15), user.avatar)
+        user_state_text = ' (online)' if user.state == 1 else ' (offline)'
+        self.nicknameText = self.createChild(text_default, (120, 38), user.nickname + user_state_text, (0, 0, 0))
         self.surface = FriendBlock.image
         self.location = location
         self.size = (350, 100)
-        self.type = type
+        self.type = block_type
+        self.frozen = False
 
     def posin(self, pos):
         x = pos[0]
@@ -39,91 +41,100 @@ class FriendBlock(Element):
         return False
 
     def getEvent(self, event):
-        if event.type == pygame.constants.MOUSEMOTION and self.state != 2:
-            if self.posin(event.pos):
-                self.state = 1
-                self.surface = FriendBlock.image_onHover
-            else:
-                self.state = 0
-                self.surface = FriendBlock.image
-            return
-        if event.type == pygame.constants.MOUSEBUTTONDOWN and event.button == pygame.constants.BUTTON_LEFT:
-            if self.posin(event.pos):
-                self.state = 2
-                self.surface = FriendBlock.image_onClick
-                print(self.user.nickname)
-            else:
-                self.state = 0
-                self.surface = FriendBlock.image
+        if not self.frozen:
+            if event.type == pygame.MOUSEMOTION and self.state != 2:
+                if self.posin(event.pos):
+                    self.state = 1
+                    self.surface = FriendBlock.image_onHover
+                else:
+                    self.state = 0
+                    self.surface = FriendBlock.image
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
+                if self.posin(event.pos):
+                    self.state = 2
+                    self.surface = FriendBlock.image_onClick
+                    print(self.user.nickname)
+                else:
+                    self.state = 0
+                    self.surface = FriendBlock.image
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_RIGHT and self.posin(event.pos):
+                self.rightClickMenu.change_location(event.pos)
+                self.rightClickMenu.set_user(self.user)
+                self.rightClickMenu.enable()
 
 
 class FriendList(Element):
     listCover = pygame.image.load('./resources/listCover.png')
 
-    def __init__(self, process, location, friendList, groupList, messageList):
+    def __init__(self, process, location, friend_list, group_list, message_list):
         Element.__init__(self, process)
         self.location = location
         self.surface = pygame.Surface((350, 600))
-        self.friendList = friendList
-        self.groupList = groupList
-        self.messageList = messageList
+        self.surface.fill((220, 220, 220))
+        self.friendList = friend_list
+        self.groupList = group_list
+        self.messageList = message_list
         self.blocks = []
-        self.menuBar = self.createChild(MenuBar, (0, 0))
+        self.displayType = 0
         self.listCoverY = -200
         self.cover = FriendList.listCover
-        for i in range(0, len(self.friendList)):
-            user = self.friendList[i]
-            self.blocks.append(self.createChild(FriendBlock, (0, i * 100 + 45), user, 1))
-        for i in range(0, len(self.groupList)):
-            user = self.groupList[i]
-            self.blocks.append(self.createChild(FriendBlock, (0, i * 100 + 45), user, 2))
-        for i in range(0, len(self.messageList)):
-            user = self.messageList[i]
-            self.blocks.append(self.createChild(FriendBlock, (0, i * 100 + 45), user, 0))
-        self.surface.fill((220, 220, 220))
+        self.rightClickMenu = self.createChild(RightClickMenu)
+
+        for i in range(0, len(friend_list)):
+            user = friend_list[i]
+            self.blocks.append(self.createChild(FriendBlock, (0, i * 100), user, 1, self.rightClickMenu))
+        for i in range(0, len(group_list)):
+            user = group_list[i]
+            self.blocks.append(self.createChild(FriendBlock, (0, i * 100), user, 2, self.rightClickMenu))
+        for i in range(0, len(message_list)):
+            user = message_list[i]
+            self.blocks.append(self.createChild(FriendBlock, (0, i * 100), user, 0, self.rightClickMenu))
         self.index = 0
 
     def display(self):
-        surface = pygame.Surface.copy(self.surface)
-        for child in self.childs:
-            if child.active:
-                surface.blit(child.display(), child.location)
+        surface = self.surface.copy()
+        for block in self.blocks:
+            if block.active:
+                surface.blit(block.display(), block.location)
+        if self.rightClickMenu.active:
+            surface.blit(self.rightClickMenu.display(), self.rightClickMenu.location)
         surface.blit(self.cover, (0, self.listCoverY))
-        surface.blit(self.menuBar.display(), self.menuBar.location)
         return surface
 
     def getEvent(self, event):
-        if event.type == pygame.constants.MOUSEBUTTONDOWN or event.type == pygame.constants.MOUSEMOTION:
+        if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION:
             event.pos = (event.pos[0] - self.location[0], event.pos[1] - self.location[1])
         for child in self.childs:
             if child.active:
                 child.getEvent(event)
 
-        if event.type == pygame.constants.MOUSEBUTTONDOWN:
-            if event.button == pygame.constants.BUTTON_WHEELDOWN and self.index <= len(self.blocks) - 6:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == pygame.BUTTON_WHEELDOWN and self.index <= len(self.blocks) - 6:
                 self.index += 1
                 for block in self.blocks:
                     block.location = (block.location[0], block.location[1] - 100)
-            if event.button == pygame.constants.BUTTON_WHEELUP and self.index > 0:
+            if event.button == pygame.BUTTON_WHEELUP and self.index > 0:
                 self.index -= 1
                 for block in self.blocks:
                     block.location = (block.location[0], block.location[1] + 100)
 
-        if event.type == pygame.constants.MOUSEBUTTONDOWN or event.type == pygame.constants.MOUSEMOTION:
+        if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION:
             event.pos = (event.pos[0] + self.location[0], event.pos[1] + self.location[1])
 
     def update(self):
         if self.listCoverY < 600:
             self.listCoverY = self.listCoverY + 8
-        activeList = 0
-        for i in range(3):
-            if self.menuBar.buttonState[i] == 2:
-                activeList = i
-        for child in self.childs:
-            if isinstance(child, FriendBlock) and child.type != activeList:
-                child.disable()
-            else:
-                child.enable()
+        if self.rightClickMenu.active:
+            for block in self.blocks:
+                block.frozen = True
+        else:
+            for block in self.blocks:
+                if block.type != self.displayType:
+                    block.disable()
+                else:
+                    block.enable()
+                    block.frozen = False
         for child in self.childs:
             if child.active:
                 child.update()
