e3c3995c1e7043387176c82d4d52fed6ea9e3b5b
ClementineZ <1415162394@qq.com>
2020-05-29 15:59:53 +0800

    修复了拖拽文件的逻辑

--- FrontEnd/Elements/Window.py
+++ FrontEnd/Elements/Window.py
@@ -5,6 +5,8 @@ import win32con
 import win32api
 import windnd
 class Window(Element):
+    def onDragFiles(self,msg):
+        print(msg) #Notice: msg is the files' name in form of BYTES ARRAY, not string.
     def __init__(self,process,caption,size,color):
         Element.__init__(self,process)
         self.color = color
@@ -16,8 +18,8 @@ class Window(Element):
         self.alpha = 255
         #self.origin = pygame.Surface.copy(self.surface)
         self.hwnd = pygame.display.get_wm_info()['window']
-
         windnd.hook_dropfiles(self.hwnd,self.onDragFiles)
+
     def display(self):
         #self.surface.fill(self.color)
         self.update()
@@ -25,18 +27,17 @@ class Window(Element):
             self.surface.blit(child.display(),child.location)
     def getMessage(self,message):
         pass
-    def onDragFiles(self,msg):
-        print(msg) #Notice: msg is the files' name in form of BYTES ARRAY, not string.
+    
     def set_alpha(self,alpha):        
         try:
             if alpha<0 or alpha>255:
                 return
-            print(hex(win32api.GetWindowLong(self.hwnd,win32con.GWL_STYLE)))
             exstyle = win32api.GetWindowLong(self.hwnd, win32con.GWL_EXSTYLE)
             if 0 == (exstyle & 0x80000):
                 exstyle |= win32con.WS_EX_LAYERED
                 win32api.SetWindowLong(self.hwnd, win32con.GWL_EXSTYLE, exstyle)
             win32gui.SetLayeredWindowAttributes(self.hwnd, 0, alpha, win32con.LWA_ALPHA)
+            
         except:
             print('[Error]Cannot set window\'s alpha. Are you using Windows OS?')
             return
