d4f583ae60ad654603a08565d0657eb9a8334983
callmewindow <847590417@qq.com>
2020-06-05 11:03:48 +0800

    更新了可变字体和可变多行字体，防止对类变量的修改

--- FrontEnd/Elements/TextAreaVariable.py
+++ FrontEnd/Elements/TextAreaVariable.py
@@ -1,19 +1,70 @@
 from FrontEnd.Elements.Element import Element
-from FrontEnd.Elements.TextArea import TextArea
 import pygame
-class TextAreaVariable(TextArea):
+class TextAreaVariable(Element):
     # 在原本基础上可以调整宽度、左右空白比例、背景图片样式、字体、字号
+    text_height_top_rate = 0.1
+    text_height_buttom_rate = 0.1
+    text_height_rate = 1 - (text_height_top_rate + text_height_buttom_rate)
+    
+    line_spacing = 10
     def __init__(self,process,location,width,borderRate,imageURL,fonttype,fontsize,text,color):
-        TextArea.font = pygame.font.SysFont(fonttype,fontsize)
+        Element.__init__(self,process)
+        self.font = pygame.font.SysFont(fonttype,fontsize)
         if imageURL == 'white':
             tempBG = pygame.Surface((width,1))
             tempBG.fill((255,255,255))
-            TextArea.bubble = tempBG
+            self.bubble = tempBG
         else:
-            TextArea.bubble = pygame.image.load(imageURL)
-        TextArea.bubble_width = width
-        TextArea.text_width_left_rate = borderRate
-        TextArea.text_width_right_rate = borderRate
-        TextArea.text_width_rate = 1 - 2*borderRate
-        TextArea.text_width = TextArea.bubble_width * TextArea.text_width_rate
-        TextArea.__init__(self,process,location,text,color)
+            self.bubble = pygame.image.load(imageURL)
+        self.bubble_width = width
+        self.text_width_left_rate = borderRate
+        self.text_width_right_rate = borderRate
+        self.text_width_rate = 1 - 2*borderRate
+        self.text_width = self.bubble_width * self.text_width_rate
+        self.location = location
+        self.surface = None
+        self.color = color
+        self.setText(text)
+        
+    def alignCenter(self,pos):
+        x = pos[0]
+        y = pos[1]
+        rect = self.surface.get_rect()
+        rectX = rect[2]
+        rectY = rect[3]
+        self.location = (x-rectX//2,y-rectY//2)
+
+    def setText(self,text):
+        lines = []
+        sentence = ''
+        text_height = 0
+        line_height = 0
+        for char in text:
+            sentence_size = self.font.size(sentence+char)
+            if line_height<sentence_size[1]:
+                line_height = sentence_size[1]
+            if sentence_size[0]>self.text_width or char=='\n':
+                lines.append(sentence)
+                print(sentence_size[0],sentence)
+                text_height += sentence_size[1] + self.line_spacing
+                if char == '\n':
+                    sentence = ''
+                else:
+                    sentence = char
+            else:
+                sentence += char
+        if sentence:
+            lines.append(sentence)
+            text_height += sentence_size[1]
+            sentence = ''
+        bubble_width = int(self.bubble_width)
+        bubble_height = int(text_height/self.text_height_rate)
+        print('Debug:bubble ',bubble_width,bubble_height)
+        self.surface = pygame.transform.smoothscale(self.bubble,(bubble_width,bubble_height))
+        drawX = self.text_width_left_rate*bubble_width
+        drawY = self.text_height_top_rate*bubble_height
+        for line in lines:
+            if line == '':
+                line = ' '
+            self.surface.blit(self.font.render(line,True,self.color),(drawX,drawY))
+            drawY += self.line_spacing + line_height
--- FrontEnd/Elements/text_variable.py
+++ FrontEnd/Elements/text_variable.py
@@ -1,13 +1,33 @@
 from FrontEnd.Elements.Element import Element
-from FrontEnd.Elements.text_default import text_default
 import pygame
 
-class text_variable(text_default):
+
+class text_variable(Element):
     def __init__(self, process, location, text, fonttype, fontsize, color):
-        text_default.font = pygame.font.SysFont(fonttype, fontsize)
+        Element.__init__(self, process)
+        self.font = pygame.font.SysFont(fonttype, fontsize)
+        self.location = location
         if text == None:
             tempText = "无"
         else:
             tempText = text
-        text_default.__init__(self, process, location, tempText, color)
-    
+        self.surface = self.font.render(tempText, True, color)
+        self.text = tempText
+        self.color = color
+
+    def alignCenter(self, pos):
+        x = pos[0]
+        y = pos[1]
+        rect = self.surface.get_rect()
+        rectX = rect[2]
+        rectY = rect[3]
+        self.location = (x-rectX//2, y-rectY//2)
+
+    def setText(self, text):
+        self.text = text
+        rect = self.surface.get_rect()
+        self.surface = self.font.render(text, True, self.color)
+        rectX = rect[2]
+        rectY = rect[3]
+        center = (self.location[0]+rectX//2, self.location[1]+rectY//2)
+        self.alignCenter(center)
