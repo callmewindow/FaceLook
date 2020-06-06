b9990a1d55d686b9088f15f9e534a6a63115849f
ClementineZ <1415162394@qq.com>
2020-06-06 13:31:38 +0800

    在Button类下新增了两个子类，关闭和最小化按钮；添加到了登录窗口中。

--- FrontEnd/Elements/Button.py
+++ FrontEnd/Elements/Button.py
@@ -1,12 +1,53 @@
 from FrontEnd.Elements.Element import Element
+from FrontEnd.Processes.SessionWindowProcess import createSession
 import pygame
-class Button():
-    def __init__(self,process):
+from Common.base import *
+class Button(Element):
+    source_img = pygame.image.load('./resources/WindowControlUI/close.png')
+    image = pygame.transform.smoothscale(source_img,(32,32))
+    bigImage = pygame.transform.smoothscale(source_img,(40,40))
+    del source_img
+    def __init__(self,process,location):
         Element.__init__(self,process)
+        self.surface = self.image        
+        self.smallSize = (32,32)
+        self.smallLocation = location
+        self.bigSize = (40,40)
+        self.bigLocation = (location[0]-(self.bigSize[0]-self.smallSize[0])/2,location[1]-(self.bigSize[1]-self.smallSize[1])/2)        
+        self.location = location
+        self.size = self.smallSize
+    def posin(self,pos):
+        x = pos[0]
+        y = pos[1]
+        if self.location[0]<=x and x<=self.location[0]+self.size[0] and self.location[1]<=y and y<=self.location[1]+self.size[1]:
+            return True
+        return False
+    def getEvent(self,event):
+        if event.type == pygame.constants.MOUSEMOTION:
+            if self.posin(event.pos):
+                self.size = self.bigSize
+                self.location = self.bigLocation
+                self.surface = self.bigImage
+            else:
+                self.size = self.smallSize
+                self.location = self.smallLocation
+                self.surface = self.image
+            return
+        if event.type == pygame.constants.MOUSEBUTTONDOWN and event.button == pygame.constants.BUTTON_LEFT and self.posin(event.pos):
+            self.onClick()
     def onClick(self):
         pass
-    def update(self):
-        pass
-    def onHover(self):
-        pass
+class CloseButton(Button):
+    def onClick(self):
+        self.process.stop()
+class MinimizeButton(Button):
+    source_img = pygame.image.load('./resources/WindowControlUI/move.png')
+    image = pygame.transform.smoothscale(source_img,(32,32))
+    bigImage = pygame.transform.smoothscale(source_img,(40,40))
+    def onClick(self):
+        self.process.minimize()
+    
+
+
+            
         
