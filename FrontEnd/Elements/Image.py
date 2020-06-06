7a7de4f5f84b07daa53eaf59e2bfbe404e73ef44
ClementineZ <1415162394@qq.com>
2020-06-06 20:05:42 +0800

    优化了Image的性能，添加了内存缓存

--- FrontEnd/Elements/Image.py
+++ FrontEnd/Elements/Image.py
@@ -4,6 +4,16 @@ from BackEnd import ImageManagement
 
 
 class Image(Element):
+    loaded_url = []
+    loaded_image = []
+    @staticmethod
+    def getLoadedImage(url):
+        if url in Image.loaded_url:
+            index = Image.loaded_url.index(url)
+            print('[Image Management]Fetched loaded image '+url+'.')
+            return Image.loaded_image[index]
+        else:
+            return None
     def __init__(self, process, location, size, url):
         Element.__init__(self, process)
         self.location = location
@@ -20,15 +30,24 @@ class Image(Element):
         #Not Ready
         self.counter = (self.counter+1)%10
         if self.counter == 1 and self.url:
+            surface = Image.getLoadedImage(self.url)
+            if surface != None:
+                self.surface = pygame.transform.smoothscale(surface, self.size)
+                self.ready = True
+                return
             surface = ImageManagement.getLocalImage('./images/'+self.url)
             if surface != None:
                 self.surface = pygame.transform.smoothscale(surface, self.size)
                 self.ready = True
+                print('[Image Management]Fetched stored image '+self.url+'.')
+                Image.loaded_url.append(self.url)
+                Image.loaded_image.append(surface)
                 return
             #Not local
             if self.called == False:
                 ImageManagement.downloadImage(self.url)
                 self.called = True
+                print('[Image Management]Start fetching remote image '+self.url+'.')
             else:
                 #called and not returned
                 pass
