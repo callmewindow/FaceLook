8c3856c2871d34c7885f3afee578f7c0b37d568e
ClementineZ <1415162394@qq.com>
2020-06-07 01:01:41 +0800

    添加了request的样例

---
+++ interface_sample.py
@@ -0,0 +1,59 @@
+login_request = {
+    'messageNumber':'2',
+    'username':'MinatoAqua',
+    'password':'MinatoAqua',
+    }
+register_request = {
+        'messageNumber':'3',
+        'username':'MinatoAqua',
+        'password':'MinatoAqua',
+        'avatarAddress':'cd37c244-6558-42de-8fd4-770f75d1be8e',
+        'phoneNumber':'404',
+        'email':'minato@aqua.com',
+        'occupation':'hololive',
+        'location':'Japan'
+        }
+get_friend_list_request = {
+        'messageNumber':'4',
+    }
+get_history_message_request = {
+        'messageNumber':'5',
+    }
+create_session_request = {
+        'messageNumber':'6',
+        'sessionName':'HoloHolo',
+    }
+take_user_into_session_request = {
+        'messageNumber':'7',
+        'username':'hcz',
+        'sessionId':'233',
+    }
+get_notice_request = {
+        'messageNumber':'8',
+    }
+send_message_request = {
+        'messageNumber':'9',
+        'sessionId':'233',
+        'from':'MinatoAqua',
+        'to':'Yagoo',
+        'time':'2020-06-07-00-52',
+        'content':'本 社 爆 破',
+        'kind':'0',
+    }
+remove_frient_request = {
+        'messageNumber':'15',
+        'username':'Yagoo',
+    }
+quit_session_request = {
+        'messageNumber':'17',
+        'sessionId':'233',
+    }
+edit_profile_request = {
+        'messageNumber':'18',
+        'nickname':'AKUKIN',
+        'avatarAddress':'116aba69-4727-41ad-948b-d4e6d98381ed',
+        'phoneNumber':'404404',
+        'email':'minato@akukin.tv',
+        'occupation':'Homolive',
+        'location':'Homo',
+    }
