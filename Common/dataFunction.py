2531ecd9b2941143e5e296d31d8134545954ffd7
ClementineZ <1415162394@qq.com>
2020-06-19 23:06:35 +0800

    修正了data的锁

--- Common/dataFunction.py
+++ Common/dataFunction.py
@@ -1,6 +1,8 @@
+def lockData(data):
+    data['write_lock'].acquire()
+def unlockData(data):
+    data['write_lock'].release()
 def readData(data):
     return data['inner']
 def writeData(data,data_copy):
-    data['write_lock'].acquire()
     data['inner'] = data_copy
-    data['write_lock'].release()
